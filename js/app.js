75facf944e5d4e85524c1f44dd3092af438eb044
Liar4u <pavlenkoliar@gmail.com>
2021-10-08 22:03:57 +0300

    fixed background behind buttons

--- js/app.js
+++ js/app.js
@@ -1,100 +1,86 @@
-class drag {
-  constructor(container) {
-    this.container = document.querySelector(container);
-    this.placeholders = [
-      ...this.container.getElementsByClassName('placeholder'),
-    ];
-    this.items = this.container.querySelectorAll('.item');
-    this.init();
+container = document.querySelector('main');
+placeholders = [...container.getElementsByClassName('placeholder')];
+items = container.querySelectorAll('.item');
+init();
+
+function init() {
+  initItems();
+  initPlaceholders(placeholders);
+  activeElement = null;
+}
+
+function initItems() {
+  items.forEach((element) => {
+    element.addEventListener('dragstart', (event) => start(event));
+    element.addEventListener('dragend', (event) => end(event));
+  });
+}
+
+function initPlaceholders(placeholders) {
+  // console.log(placeholders.length);
+  for (let i in placeholders) {
+    placeholders[i].addEventListener('dragover', (event) => over(event));
+    placeholders[i].addEventListener('dragenter', (event) => enter(event));
+    placeholders[i].addEventListener('dragleave', (event) => leave(event));
+    placeholders[i].addEventListener('drop', (event) => drop(event));
   }
+}
 
-  init() {
-    this.initItems();
-    this.initPlaceholders(this.placeholders);
-    this.activeElement = null;
+function itIsLegal(targetObject) {
+  const item = activeElement.classList;
+  const target = targetObject.target.classList;
+  if (target.contains('start')) {
+    return item.contains('finish') || item.contains('pause') ? true : false;
+  } else if (target.contains('pause') || target.contains('placeholder')) {
+    return item.contains('start') || item.contains('finish') ? true : false;
+  } else {
+    return false;
   }
+}
 
-  initItems() {
-    this.items.forEach((element) => {
-      element.addEventListener('dragstart', (event) => this.start(event));
-      element.addEventListener('dragend', (event) => this.end(event));
-    });
+function start(event) {
+  const taskId = String(event.target.id);
+  const parent = document.getElementById(taskId);
+  event.target.classList.add('hold');
+  activeElement = event.target;
+  parent.closest('.placeholder').classList.remove('closed');
+}
+
+function end(event) {
+  event.target.classList.remove('hold', 'hide');
+}
+
+function over(event) {
+  if (itIsLegal(event)) {
+    event.preventDefault();
   }
+}
 
-  initPlaceholders(placeholders) {
-    for (let i in placeholders) {
-      placeholders[i].addEventListener('dragover', (event) => this.over(event));
-      placeholders[i].addEventListener('dragenter', (event) =>
-        this.enter(event)
-      );
-      placeholders[i].addEventListener('dragleave', (event) =>
-        this.leave(event)
-      );
-      placeholders[i].addEventListener('drop', (event) => this.drop(event));
-    }
+function enter(event) {
+  if (
+    !event.target.classList.contains('closed') &&
+    event.target.classList.contains('placeholder')
+  ) {
+    event.target.classList.add('hovered');
   }
+}
 
-  itIsLegal(targetObject) {
-    const item = this.activeElement.classList;
-    const target = targetObject.target.classList;
-    if (target.contains('start')) {
-      return item.contains('finish') || item.contains('pause') ? true : false;
-    } else if (target.contains('pause') || target.contains('placeholder')) {
-      return item.contains('start') || item.contains('finish') ? true : false;
-    } else {
-      return false;
-    }
+function leave(event) {
+  event.target.classList.remove('hovered');
+}
+
+function drop(event) {
+  const holdCopy = document.querySelector('.hold').cloneNode(true);
+  if (itIsLegal(event) && !event.target.classList.contains('placeholder')) {
+    event.target.replaceWith(holdCopy);
+  } else {
+    event.target.append(holdCopy);
   }
-
-  start(event) {
-    const taskId = String(event.target.id);
-    const parent = document.getElementById(taskId);
-    event.target.classList.add('hold');
-    this.activeElement = event.target;
-    parent.closest('.placeholder').classList.remove('closed');
-  }
-
-  end(event) {
-    event.target.classList.remove('hold', 'hide');
-  }
-
-  over(event) {
-    if (this.itIsLegal(event)) {
-      event.preventDefault();
-    }
-  }
-
-  enter(event) {
-    if (
-      !event.target.classList.contains('closed') &&
-      event.target.classList.contains('placeholder')
-    ) {
-      event.target.classList.add('hovered');
-    }
-  }
-
-  leave(event) {
-    event.target.classList.remove('hovered');
-  }
-
-  drop(event) {
-    const holdCopy = document.querySelector('.hold').cloneNode(true);
-
-    if (
-      this.itIsLegal(event) &&
-      !event.target.classList.contains('placeholder')
-    ) {
-      event.target.replaceWith(holdCopy);
-    } else {
-      event.target.append(holdCopy);
-    }
-
-    event.target.classList.remove('hovered');
-    event.target.classList.add('closed');
-    holdCopy.classList.remove('col-header', 'hold');
-    holdCopy.setAttribute('draggable', 'false');
-    holdCopy.style.cursor = 'unset';
-  }
+  event.target.classList.remove('hovered');
+  event.target.classList.add('closed');
+  holdCopy.classList.remove('col-header', 'hold');
+  holdCopy.setAttribute('draggable', 'false');
+  holdCopy.style.cursor = 'unset';
 }
 
 // BTN Logic
@@ -121,12 +107,10 @@ function addNewTask() {
   document.querySelector('.task').append(div);
   let placeholders = [...document.getElementsByClassName('placeholder')];
   let targetPlaceholder = placeholders[placeholders.length - 1];
-  drag.placeholders = targetPlaceholder;
-  drag.initPlaceholders;
+  placeholders = [...targetPlaceholder];
+  initPlaceholders(placeholders);
   // document.querySelector('.task').lastChild;
   // drag.placeholders = placeholders
   // drag.initPlaceholders;
 }
 // document.getElementById('posts').appendChild(div);
-
-new drag('main');
